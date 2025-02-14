74d363e2d0c2987210e57d9dae5de510d9923ef1
ishanahmad90 <ishankhn7860@gmail.com>
2025-02-14 18:14:35 +0530

    Update code

--- lib/sheets/new_expense_sheet.dart
+++ lib/sheets/new_expense_sheet.dart
@@ -8,8 +8,65 @@ class AddNewExpense extends StatefulWidget {
 }
 
 class _AddNewExpense extends State<AddNewExpense> {
+  final TextEditingController _titleEditingController = TextEditingController();
+  final TextEditingController _amountEditingController =
+      TextEditingController();
+  DateTime? _pickedDate;
+  void _datePicker() async {
+    final now = DateTime.now();
+    final DateTime firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+        context: context, firstDate: firstDate, lastDate: now);
+    setState(() {
+      _pickedDate = pickedDate;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleEditingController,
+            maxLength: 50,
+            decoration: InputDecoration(
+              label: Text("Title"),
+              hintText: "Enter The Ttile of the Expense",
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountEditingController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    prefixText: "\$",
+                    hintText: "0.0",
+                    label: Text("Amount"),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(_pickedDate == null
+                      ? "No Date selected"
+                      : _pickedDate.toString()),
+                  IconButton(
+                    onPressed: _datePicker,
+                    icon: const Icon(Icons.calendar_month),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Text(_titleEditingController.text),
+        ],
+      ),
+    );
   }
 }
